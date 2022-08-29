b6d19f24d1badfb72bd7ff431e6c221a3d75f827
Kleinoth, Tim <tim.kleinoth@accso.de>
2022-08-29 14:49:40 +0200

    add own cnn conv matrix

--- main.py
+++ main.py
@@ -21,11 +21,17 @@ def main():
 
     trainer = Trainer(config)
 
-    trainer.train(data_aug="brightness")
+    # trainer.train(data_aug="brightness")
     # trainer.loadLogFile('training_csv.log')
     # trainer.showConfusionMatrix(Utils().getModelDirPath() + 'best_model_vgg_brightness.hdf5', "vgg_Confusion_matrix_brightness.png")
-    trainer.evaluate_saveWrongPredictions(Utils().getModelDirPath() + 'best_model_vgg.hdf5', wrong_rock=True,
-                                          modelname='vgg')
+
+    # trainer.showConfusionMatrix(Utils().getModelDirPath() + 'best_model_vgg_brightness.hdf5', "vgg_Confusion_matrix_brightness.png")
+    trainer.showConfusionMatrix(Utils().getModelDirPath() + 'best_model_cnn.hdf5', "cnn_Confusion_matrix.png")
+
+    trainer.evaluate_saveWrongPredictions(Utils().getModelDirPath() + 'best_model_cnn.hdf5', wrong_rock=False,
+                                          modelname='cnn')
+    # trainer.evaluate_saveWrongPredictions(Utils().getModelDirPath() + 'best_model_vgg.hdf5', wrong_rock=True,
+    #                                       modelname='vgg')
     # trainer.evaluate_saveWrongPredictions(Utils().getModelDirPath() + 'best_model_resnet.hdf5', wrong_rock=True,
     #                                       modelname='resnet')
 
